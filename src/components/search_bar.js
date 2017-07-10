fb11089979e6f6a191d5e13c1276d964d12405c1
Tiago de A Fernandes <tfernandes@mprj.mp.br>
2017-07-10 13:26:56 -0300

    added videos thumbnails

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'React';
+import React, {Component} from 'React';
 
 
 class SearchBar extends Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = { term: '' };
-	}
+    this.state = {term: ''};
+  }
 
-	render() {
-		return (
-			<div>
-				<input
-					value={this.state.term}
-					onChange={event => this.setState({ term: event.target.value })} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
+      </div>
+    );
+  }
 }
-
-
 
 
 export default SearchBar;
